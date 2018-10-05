2edafd6b473792c9662162ed0fb47c8361144939
yazid <yazid.zibem@gmail.com>
2018-10-05 16:02:02 +0200

    router and component (#4)
    * router and component
    
    * angular material class

---
+++ src/app/home-artist-user/home-artist-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-artist-user',
+  templateUrl: './home-artist-user.component.html',
+  styleUrls: ['./home-artist-user.component.css']
+})
+export class HomeArtistUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
