459af28aaf63a1c407d50d35b37a250819b09f20
Treille Morgane <treillemorgane@gmail.com>
2018-10-09 12:09:54 +0200

    page Création d'event

--- src/app/CustomMaterialComponent.ts
+++ src/app/CustomMaterialComponent.ts
@@ -2,15 +2,17 @@ import {
   MatButtonModule, MatCardModule,
   MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatIconModule,
   MatInputModule,
-  MatListModule, MatMenuModule,
+  MatListModule, MatMenuModule, MatNativeDateModule,
   MatProgressBarModule, MatSelectModule, MatToolbarModule
 } from '@angular/material';
 import {NgModule} from "@angular/core";
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule,MatProgressBarModule,MatListModule,MatInputModule,MatFormFieldModule,
-    MatSelectModule,MatCheckboxModule,MatDatepickerModule,MatToolbarModule,MatMenuModule,MatChipsModule,MatIconModule,MatDialogModule,MatCardModule],
+    MatSelectModule,MatCheckboxModule,MatDatepickerModule,MatToolbarModule,MatMenuModule,MatChipsModule,MatIconModule,MatDialogModule,MatCardModule,
+    MatNativeDateModule],
   exports: [MatButtonModule, MatCheckboxModule,MatProgressBarModule,MatListModule,MatInputModule,MatFormFieldModule,
-    MatSelectModule,MatCheckboxModule,MatDatepickerModule,MatToolbarModule,MatMenuModule,MatChipsModule,MatIconModule,MatDialogModule,MatCardModule],
+    MatSelectModule,MatCheckboxModule,MatDatepickerModule,MatToolbarModule,MatMenuModule,MatChipsModule,MatIconModule,MatDialogModule,MatCardModule,
+    MatNativeDateModule],
 })
 export class CustomMaterialComponent { }
