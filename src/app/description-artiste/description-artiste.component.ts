22aa584c7b688bc5f30c80bd31dde8c00a7b080a
MorganeTreille <40758057+MorganeTreille@users.noreply.github.com>
2018-10-09 10:21:18 +0200

    artistpage for user version visuel (#7)

---
+++ src/app/description-artiste/description-artiste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-artiste',
+  templateUrl: './description-artiste.component.html',
+  styleUrls: ['./description-artiste.component.css']
+})
+export class DescriptionArtisteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
