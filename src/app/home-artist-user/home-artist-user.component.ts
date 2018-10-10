83f09592f2e35bba594ce4e1ec8dffe7062b5124
Nicolas <nicolas.bachir1@gmail.com>
2018-10-10 17:17:46 +0200

    maj menu/header

--- src/app/home-artist-user/home-artist-user.component.ts
+++ src/app/home-artist-user/home-artist-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home-artist-user',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeArtistUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  login = 111;
+
+  navigateToHomeArtiste() {
+    this.router.navigate(['home/' + this.login]);
+  }
 
   ngOnInit() {
   }
