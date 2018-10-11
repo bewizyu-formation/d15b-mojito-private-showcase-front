eb6c3a9829f5e906670254205dc9709e5542a3af
Nicolas <nicolas.bachir1@gmail.com>
2018-10-11 14:12:24 +0200

    fix e2e errors and manage environment

--- src/app/services/ApiService.ts
+++ src/app/services/ApiService.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
-const detailUrl = 'https://api.github.com/users/nartawak';
+import {environment} from '../../environments/environment';
 
 @Injectable ({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getLogin(username: string, password: string): Promise<any> {
-    return this.http.post(`${this.env.getPrivateShowcaseApiConfig().uri}${RESOURCES_LOGIN}`, {
+    return this.http.post(`${environment.API_BASE_URL}/login`, {
         username,
         password,
       },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_BASE_URL : 'https://d15b-private-showcase-mojito-api.cleverapps.io'
 };
