bcccf31fb7c9fab932ad3dcf35e84e3b15f6f1f4
yazid <yazid.zibem@gmail.com>
2018-10-15 15:05:43 +0200

    appel reseau login (#16)

--- src/app/services/LoginService.ts
+++ src/app/services/LoginService.ts
@@ -9,17 +9,16 @@ export class LoginService {
 
   user: User;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) {
+  }
 
-  addUserLogin() {
-    return new Promise( (resolve) => {
-      this.api.getLogin(this.user.username, this.user.password)
-        .then(() => {
-          resolve(this.user);
-        })
-        .catch(error => console.log('Error : ', error));
+  addUserLogin(username , password) {
+    return new Promise((resolve) => {
+        this.api.getLogin(username, password)
+          .then()
+          .catch(error => console.log('Error : ', error));
       }
-
     );
   }
 }
+
