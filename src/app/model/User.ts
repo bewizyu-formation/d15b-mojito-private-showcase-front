e40116e1bcc2b0f6311dc1ea02ee3dce96d8c684
Nicolas <nicolas.bachir1@gmail.com>
2018-10-15 15:08:51 +0200

    minor changement in css and inrementation

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -3,22 +3,27 @@ export class User {
   private _password: string;
   private _email: string;
   private _ville: string;
+
   constructor(_username?: string, _password?: string, _email?: string, _ville?: string) {
     this._username = _username;
     this._password = _password;
     this._email = _email;
     this._ville = _ville;
   }
+
   get username(): string {
     return this._username;
   }
+
   set username(value: string) {
     console.log('SET _username', value);
     this._username = value;
   }
+
   get password(): string {
     return this._password;
   }
+
   set password(value: string) {
     console.log('SET PASSWORD', value);
     this._password = value;
